3c5aa3d0577e1cbe7f3de6df42b2f0abf6ae838b
Soujanya-A <soujanyakulal94@gmail.com>
2025-06-11 12:36:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,6 @@ def delete(id):
     return redirect('/')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if not set
+    app.run(host='0.0.0.0', port=port)
+
